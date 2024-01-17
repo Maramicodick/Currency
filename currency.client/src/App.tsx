60e4c0ddaac8e788c3fdf3473c870ebd118cd446
Maramicodick <sashamacinovich@gmail.com>
2024-01-17 17:35:08 +0300

    date remake in graph

--- currency.client/src/App.tsx
+++ currency.client/src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 import axios from 'axios';
 import { Currency } from './models/Currency';
 import { Header } from 'semantic-ui-react';
-import { CartesianGrid, Line, LineChart, Tooltip, XAxis } from 'recharts';
+import { CartesianGrid, Area, AreaChart, Tooltip, XAxis } from 'recharts';
+import moment from 'moment';
 
 function App() {
     const [currencies, setCurrencies] = useState<Currency[]>([]);
@@ -15,15 +16,19 @@ function App() {
             })
     }, [])
 
+    const formatXAxis = (tickItem : Date) => {
+        return moment(tickItem).format("DD/MM");
+    }
+
     return (
         <div>
             <Header as="h2" content='BTC to USD' />
-            <LineChart width={400} height={400} data={currencies} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
-                <XAxis dataKey="date" />
+            <AreaChart width={ 1200 } height={400} data={currencies} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
+                <XAxis dataKey="date" tickFormatter={(tick) => formatXAxis(tick)} />
                 <Tooltip />
                 <CartesianGrid stroke="#f5f5f5" />
-                <Line type="monotone" dataKey="exchangeRate" stroke="#ff7300" yAxisId={0} />
-            </LineChart>
+                <Area type="monotone" dataKey="exchangeRate" stroke="#ff7300" yAxisId={0} />
+            </AreaChart>
         </div>
     )
 }
